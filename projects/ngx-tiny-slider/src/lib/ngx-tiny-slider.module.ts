af39b8c179b014f744ff5dc41a6eeacf0a84c9c6
volodymyrdanyliv <vdanyliv.work@gmail.com>
2019-01-17 22:57:08 +0200

    1. Implemented wrapper, extended config interface, added some styles for navigation. 2. Imported into test component

--- projects/ngx-tiny-slider/src/lib/ngx-tiny-slider.module.ts
+++ projects/ngx-tiny-slider/src/lib/ngx-tiny-slider.module.ts
@@ -1,17 +1,23 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
 import {NgxTinySliderComponent} from './ngx-tiny-slider.component';
+
 import {NgxTinySliderService} from './ngx-tiny-slider.service';
 
 @NgModule({
-  declarations: [
-    NgxTinySliderComponent
-  ],
-  imports: [
-    NgxTinySliderService
-  ],
-  exports: [
-    NgxTinySliderComponent
-  ]
+    imports: [
+      CommonModule
+    ],
+    declarations: [
+        NgxTinySliderComponent
+    ],
+    providers: [
+        NgxTinySliderService
+    ],
+    exports: [
+        NgxTinySliderComponent
+    ]
 })
 export class NgxTinySliderModule {
 }
