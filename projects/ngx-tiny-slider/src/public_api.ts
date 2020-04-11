88f3152e7e26480c31ff24043b6d808d393e4b14
deniska <denis.yesakov@gmail.com>
2020-04-11 23:45:35 +0200

    upgrade to angular 9

--- projects/ngx-tiny-slider/src/public_api.ts
+++ projects/ngx-tiny-slider/src/public_api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ngx-tiny-slider
  */
 
+export * from './lib/ngx-tiny-slider.component';
 export * from './lib/ngx-tiny-slider.module';
 export * from './lib/interfaces/ngx-tiny-slider-settings.interface';
