5d4379f2c5bd802d5e91e8b113dbe544dba7b8f9
volodymyrdanyliv <vdanyliv.work@gmail.com>
2020-04-12 13:06:18 +0300

    updated demo example with new version of module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {NgxTinySliderModule} from '../../projects/ngx-tiny-slider/src/lib/ngx-tiny-slider.module';
+import {NgxTinySliderModule} from 'ngx-tiny-slider';
 
 @NgModule({
   imports: [
